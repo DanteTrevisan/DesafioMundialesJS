d69a24fa04da393afee64d6766a8e2aa2682efb9
Dante Trevisan <dante.trevisan248@gmail.com>
2024-07-26 12:44:15 -0300

    primer commit

--- js/script.js
+++ js/script.js
@@ -1,36 +1,56 @@
-function sumarSinArgumentos(){
-    let numero1 = 5;
-    let resultado = numero1+5;
-
-    return resultado
+let usuario;
+let numero2;
+let numero1 = prompt("Ingrese el primer numero");
+if (numero1 !== "F" && numero1 !== "f"){
+    usuario = prompt("Ingrese que operacion matematica desea realizar con los simbolos de +, -, *, /, ^(potencia), %(modulo o resto). Para salir ingrese 'f' o 'F' (sin comillas)");
+    if (usuario !== "F" && usuario !== "f"){
+        numero2 = prompt("Ingrese el segundo numero");
+        if (numero2 === "F" || numero2 === "f"){
+            alert("Saliendo")
+        }
+    }
+    else{
+        alert("Saliendo")
+    }
+}else{
+    alert("Saliendo")
 }
+while(usuario !== "F" && usuario !== "f" && `${numero1}` !== "f" && `${numero2}` !== "f" && `${numero1}` !== "F" && `${numero2}` !== "F") {
+    numero1 = parseFloat(numero1);
+    numero2 = parseFloat(numero2);
+    if (usuario == "+"){
+        alert(`${numero1} + ${numero2} = ${numero1 + numero2}`);
+    }else if(usuario == "-"){
+        alert(`${numero1} - ${numero2} = ${numero1 - numero2}`);
+    }else if(usuario == "*"){
+        alert(`${numero1} * ${numero2} = ${numero1 * numero2}`);
+    }else if(usuario == "/" && numero2 == 0){
+        alert("Division por cero, ingrese un numero DISTINTO de cero");
+        numero2 = parseFloat(prompt("Ingrese el segundo numero"));
+        alert(`${numero1} / ${numero2} = ${numero1 / numero2}`);
+    }else if(usuario == "%"){
+        alert(`${numero1} % ${numero2} = ${numero1 % numero2}`);
+    }else if(usuario == "^"){
+        alert(`${numero1} ^ ${numero2} = ${numero1 ** numero2}`);
+    }else{
+        alert("Operador ingresado incorrecto, ingrese +, -, *, /, ^(potencia), %(modulo o resto). Para salir ingrese 'f' o 'F' (sin comillas)")
+    }
+    
+    numero1 = prompt("Ingrese el primer numero");
+    if (numero1 !== "F" && numero1 !== "f"){
+        usuario = prompt("Ingrese que operacion matematica desea realizar con los simbolos de +, -, *, /, ^(potencia), %(modulo o resto). Para salir ingrese 'f' o 'F' (sin comillas)");
+        if (usuario !== "F" && usuario !== "f"){
+            numero2 = prompt("Ingrese el segundo numero");
+            if (numero2 === "F" || numero2 === "f"){
+                alert("Saliendo")
+            }
+        }
+        else{
+            alert("Saliendo")
+        }
+    }else{
+        alert("Saliendo")
+    }
 
-console.log("Funcion escrita normal sin argumentos")
-console.log(sumarSinArgumentos())
-
-let sumarFlechaSinArgumentos = () => {
-    let numero1 = 5;
-    let resultado = numero1+5;
-
-    return resultado; 
 }
-
-console.log("Funcion flecha sin argumentos");
-console.log(sumarFlechaSinArgumentos());
-
-function sumarConArgumentos(numero1, numero2){
-    let resultado = numero1+numero2;
-    return resultado;
-}
-
-console.log("Funcion con argumentos");
-console.log(sumarConArgumentos(100,50));
-
-let sumarFlechaConArgumentos = (numero1, numero2) => {
-    let resultado = numero1+numero2;
-    return resultado; 
-}
-
-console.log("Funcion flecha con argumentos");
-console.log(sumarFlechaConArgumentos(200,300));
 
