50eb3a0890f6092131af0b17eccbef612ecc6869
Dante Trevisan <dante.trevisan248@gmail.com>
2024-07-26 13:41:02 -0300

    corregido errores de validacion

--- js/script.js
+++ js/script.js
@@ -1,24 +1,10 @@
 debugger;
-let usuario;
+let usuario = prompt("Ingrese que operacion matematica desea realizar con los simbolos de +, -, *, /, ^(potencia), %(modulo o resto). Para salir ingrese 'f' o 'F' (sin comillas)");
+let numero1;
 let numero2;
-let numero1 = prompt("Ingrese el primer numero");
-if (numero1 != "F" && numero1 != "f") {
-    usuario = prompt("Ingrese que operacion matematica desea realizar con los simbolos de +, -, *, /, ^(potencia), %(modulo o resto). Para salir ingrese 'f' o 'F' (sin comillas)");
-    if (usuario !== "F" && usuario != "f") {
-        numero2 = prompt("Ingrese el segundo numero");
-        if (numero2 == "F" || numero2 == "f") {
-            alert("Saliendo")
-        }
-    }
-    else {
-        alert("Saliendo")
-    }
-} else {
-    alert("Saliendo")
-}
-while (usuario != "F" && usuario != "f" && numero1 != "f" && numero2 != "f" && numero1 != "F" && numero2 != "F" && usuario == "/") {
-    numero1 = parseFloat(numero1);
-    numero2 = parseFloat(numero2);
+while (usuario != "F" && usuario != "f") {
+    numero1 = parseFloat(prompt("Ingrese el primer numero"));
+    numero2 = parseFloat(prompt("Ingrese el segundo numero"));
     if (usuario == "+") {
         alert(`${numero1} + ${numero2} = ${numero1 + numero2}`);
     } else if (usuario == "-") {
@@ -40,21 +26,9 @@ while (usuario != "F" && usuario != "f" && numero1 != "f" && numero2 != "f" && n
         alert("Operador ingresado incorrecto, ingrese +, -, *, /, ^(potencia), %(modulo o resto). Para salir ingrese 'f' o 'F' (sin comillas)")
     }
 
-    numero1 = prompt("Ingrese el primer numero");
-    if (numero1 !== "F" && numero1 !== "f") {
-        usuario = prompt("Ingrese que operacion matematica desea realizar con los simbolos de +, -, *, /, ^(potencia), %(modulo o resto). Para salir ingrese 'f' o 'F' (sin comillas)");
-        if (usuario != "F" && usuario != "f") {
-            numero2 = prompt("Ingrese el segundo numero");
-            if (numero2 === "F" || numero2 === "f") {
-                alert("Saliendo")
-            }
-        }
-        else {
-            alert("Saliendo")
-        }
-    } else {
-        alert("Saliendo")
-    }
-
+    usuario = prompt("Ingrese que operacion matematica desea realizar con los simbolos de +, -, *, /, ^(potencia), %(modulo o resto). Para salir ingrese 'f' o 'F' (sin comillas)");
+       
 }
+
+alert("Saliendo");
 
