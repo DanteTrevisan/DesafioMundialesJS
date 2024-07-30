7e7af13cdc612d30fabcad88197d5d1824fc68c2
Dante Trevisan <dante.trevisan248@gmail.com>
2024-07-30 12:00:56 -0300

    corregido validaciones

--- js/script.js
+++ js/script.js
@@ -1,13 +1,9 @@
 let edad = parseInt(prompt("Ingrese su edad"));
 
-if (edad == 84){
-    alert("Vivio exactamente 21 mundiales")
+if (edad % 4 == 0){
+    alert(`Vivio exactamente ${edad/4} mundiales`)
 }else{
-    let resto = edad/4 - Math.floor(edad/4);
-    if (resto <= .5){
-        alert(`VIvio ${Math.floor(edad/4)} Mundiales`);
-    }else{
-        alert(`VIvio ${Math.ceil(edad/4)} Mundiales`);
-    }
+    alert(`VIvio ${Math.floor(edad/4)} Mundiales`);
+
 }
 
