7d1e858e86322360d427f4846dceea09bba2ab05
Dante Trevisan <dante.trevisan248@gmail.com>
2024-07-30 11:38:22 -0300

    primer commit

--- js/script.js
+++ js/script.js
@@ -1,33 +1,13 @@
-let usuario = prompt("Ingrese que operacion matematica desea realizar con los simbolos de +, -, *, /, ^(potencia), %(modulo o resto). Para salir ingrese 'f' o 'F' (sin comillas)");
-let numero1;
-let numero2;
-while (usuario != "F" && usuario != "f") {
-    numero1 = parseFloat(prompt("Ingrese el primer numero"));
-    numero2 = parseFloat(prompt("Ingrese el segundo numero"));
-    if (usuario == "+") {
-        alert(`${numero1} + ${numero2} = ${numero1 + numero2}`);
-    } else if (usuario == "-") {
-        alert(`${numero1} - ${numero2} = ${numero1 - numero2}`);
-    } else if (usuario == "*") {
-        alert(`${numero1} * ${numero2} = ${numero1 * numero2}`);
-    } else if (usuario == "/") {
-        if (numero2 == 0) {
-            alert("Division por cero, ingrese un numero DISTINTO de cero");
-            numero2 = parseFloat(prompt("Ingrese el segundo numero"));
-            alert(`${numero1} / ${numero2} = ${numero1 / numero2}`);
-        }
-        alert(`${numero1} / ${numero2} = ${numero1 / numero2}`);
-    } else if (usuario == "%") {
-        alert(`${numero1} % ${numero2} = ${numero1 % numero2}`);
-    } else if (usuario == "^") {
-        alert(`${numero1} ^ ${numero2} = ${numero1 ** numero2}`);
-    } else {
-        alert("Operador ingresado incorrecto, ingrese +, -, *, /, ^(potencia), %(modulo o resto). Para salir ingrese 'f' o 'F' (sin comillas)")
+let edad = parseInt(prompt("Ingrese su edad"));
+
+if (edad == 84){
+    alert("Vivio exactamente 21 mundiales")
+}else{
+    let resto = edad/4 - Math.floor(edad/4);
+    if (resto <= .5){
+        alert(`VIvio ${Math.floor(edad/4)} Mundiales`);
+    }else{
+        alert(`VIvio ${Math.ceil(edad/4)} Mundiales`);
     }
-
-    usuario = prompt("Ingrese que operacion matematica desea realizar con los simbolos de +, -, *, /, ^(potencia), %(modulo o resto). Para salir ingrese 'f' o 'F' (sin comillas)");
-       
 }
-
-alert("Saliendo"); 
 
